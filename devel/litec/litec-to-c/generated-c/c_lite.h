861918febe0d3b55300048fb57efdbf274464a15
luciotato <luciotato@gmail.com>
2014-08-02 04:31:24 -0300

    remove generated-c files

--- devel/litec/litec-to-c/generated-c/c_lite.h
+++
@@ -1,8 +0,0 @@
-#ifndef C_LITE_C__H
-#define C_LITE_C__H
-#include "_dispatcher.h"
-//-------------------------
-//Module c_lite
-//-------------------------
-extern void c_lite__moduleInit(void);
-#endif
--- devel/litec/litec-to-js/generated-c/Producer_js.h
+++
@@ -1,8 +0,0 @@
-#ifndef PRODUCER_JS_C__H
-#define PRODUCER_JS_C__H
-#include "_dispatcher.h"
-//-------------------------
-//Module Producer_js
-//-------------------------
-extern void Producer_js__moduleInit(void);
-#endif
